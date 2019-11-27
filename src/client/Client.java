50c471dd97bac2dbbcdd3e1a7bca5901b3261436
Pedro Carrega <pedro.carrega@outlook.com>
2019-11-27 23:26:23 +0000

    implementing menu on client

--- src/client/Client.java
+++ src/client/Client.java
@@ -2,6 +2,9 @@ package client;
 
 import java.util.List;
 import java.util.Scanner;
+
+import javax.print.DocFlavor.INPUT_STREAM;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -10,37 +13,62 @@ import java.net.UnknownHostException;
 import java.util.ArrayList;
 
 public class Client {
-	
+
 	private List<Integer> clientes = new ArrayList<Integer>();
-	
+
 	public static void main(String[] args) throws NumberFormatException, UnknownHostException, ClassNotFoundException, IOException {
-		
-		conectionServer(args[0], Integer.parseInt(args[1]));
-	
+
+		Scanner sc = new Scanner(System.in);
+
+		while(true) {
+			String input = sc.nextLine();
+			
+			switch (Integer.parseInt(input)) {
+			case 1:
+				conectionServer(args[0], Integer.parseInt(args[1]), sc);
+				//agora ligar ao stream
+				break;
+			
+			case 2:
+				//I want to stream
+				break;
+				
+			default:
+				//caso input invalido fechar aplicacao?
+				input = "-1";
+				break;
+			}
+			
+
+			if(input.equals("-1")) {
+				sc.close();
+				break;
+			}
+		}
+
 	}
-	public static void conectionServer(String ip, int port) throws UnknownHostException, IOException, ClassNotFoundException{
+	public static String conectionServer(String ip, int port, Scanner sc) throws UnknownHostException, IOException, ClassNotFoundException{
 		Socket cliente = new Socket(ip, port);
 
 		ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
 		ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
-		
+
 		String opcoes = (String) entrada.readObject();
 		System.out.println(opcoes);
-		
-		Scanner sc = new Scanner(System.in);
+
 		System.out.println("Escreva o canal que pretende visualizar das opções acima listadas");
-		
+
 		String opcao = sc.nextLine();
-		
+
 		saida.writeObject(opcao);
-		String idk = (String) entrada.readObject();
-		
-		System.out.println(idk);		
-		
+		String result = (String) entrada.readObject();
+
+		System.out.println(result);		
+
 		cliente.close();
 		saida.close();
-		sc.close();
-		//byte [] b = InetAddress.getByName("localhost"). getAddress();
 		entrada.close();
+		
+		return result;
 	}
 }
