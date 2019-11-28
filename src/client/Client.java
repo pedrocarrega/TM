e6573f02e867d5118d5045c8106697e840a304a5
Pedro Carrega <pedro.carrega@outlook.com>
2019-11-28 18:49:50 +0000

    moved some stuff around and implemented some methods

--- src/client/Client.java
+++ src/client/Client.java
@@ -8,65 +8,96 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
 public class Client {
 
-	private List<Integer> clientes = new ArrayList<Integer>();
+	private static List<String> clients;
 
 	public static void main(String[] args) throws NumberFormatException, UnknownHostException, ClassNotFoundException, IOException {
 
 		Scanner sc = new Scanner(System.in);
-
-		while(true) {
-			String input = sc.nextLine();
-			
-			switch (Integer.parseInt(input)) {
-			case 1:
-				conectionServer(args[0], Integer.parseInt(args[1]), sc);
-				//agora ligar ao stream
-				break;
-			
-			case 2:
-				//I want to stream
-				break;
-				
-			default:
-				//caso input invalido fechar aplicacao?
-				input = "-1";
-				break;
-			}
-			
-
-			if(input.equals("-1")) {
-				sc.close();
-				break;
-			}
-		}
+		int input = Integer.parseInt(sc.nextLine());
+		
+		if(1 <= input && input <= 3)
+			connectServer(args[0], Integer.parseInt(args[1]), input, sc);
+		else
+			System.err.println("INVALID ACTION");
 
 	}
-	public static String conectionServer(String ip, int port, Scanner sc) throws UnknownHostException, IOException, ClassNotFoundException{
-		Socket cliente = new Socket(ip, port);
 
-		ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
-		ObjectInputStream entrada = new ObjectInputStream(cliente.getInputStream());
+	private static void connectServer(String ip, int port, int action, Scanner sc) throws UnknownHostException, IOException, ClassNotFoundException{
+		Socket socket = new Socket(ip, port);
 
-		String opcoes = (String) entrada.readObject();
+		ObjectOutputStream outStream = new ObjectOutputStream(socket.getOutputStream());
+		ObjectInputStream inStream = new ObjectInputStream(socket.getInputStream());
+
+		switch (action) {
+		case 1:
+			String host = getHost(inStream, outStream, sc);
+			socket.close();
+			outStream.close();
+			inStream.close();
+			if(host != null)
+				connectStream(host);
+			break;
+
+		case 2:
+			stream();
+			socket.close();
+			outStream.close();
+			inStream.close();
+			break;
+
+		}
+
+
+
+		
+
+
+	}
+
+	private static void connectStream(String host) throws NumberFormatException, UnknownHostException, IOException, ClassNotFoundException {
+		
+		String[] info = host.split(":");
+		
+		
+		Socket socket = new Socket(info[0], Integer.parseInt(info[1]));
+		//ObjectOutputStream outStream = new ObjectOutputStream(socket.getOutputStream());
+		ObjectInputStream inStream = new ObjectInputStream(socket.getInputStream());
+		clients = new ArrayList<String>();
+		
+		LocalTime timer = LocalTime.now();
+		
+		while(LocalTime.now().getSecond()-timer.getSecond()<60) {
+			System.out.println((String)inStream.readObject());
+		}
+		
+		socket.close();
+		inStream.close();
+		
+	}
+
+	private static String getHost(ObjectInputStream in, ObjectOutputStream out, Scanner sc) throws ClassNotFoundException, IOException {
+		
+		String opcoes = (String) in.readObject();
 		System.out.println(opcoes);
 
 		System.out.println("Escreva o canal que pretende visualizar das opções acima listadas");
 
 		String opcao = sc.nextLine();
 
-		saida.writeObject(opcao);
-		String result = (String) entrada.readObject();
-
-		System.out.println(result);		
-
-		cliente.close();
-		saida.close();
-		entrada.close();
+		out.writeObject(opcao);
+		String result = (String) in.readObject();
+		System.out.println(result);
 		
 		return result;
+		
+	}
+
+	private static void stream() {
+
 	}
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -12,9 +12,10 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 public class Server {
-	
+
 	private static Map<Integer, List<String>> transmiters = new HashMap<Integer, List<String>>();
 	private static Map<Integer, Integer> counter = new HashMap<Integer, Integer>();
+
 
 	public static void main(String[] args) throws IOException {
 
@@ -22,12 +23,12 @@ public class Server {
 		testFillMaps();
 		Server server = new Server();
 		server.startServer(args[0]);
-		
+
 
 	}
 
 	private static void testFillMaps() {
-		
+
 		List<String> test = new ArrayList<String>();
 		test.add("300:600");
 
@@ -37,44 +38,61 @@ public class Server {
 		test.add("666:300");
 		transmiters.put(54321, test);
 		counter.put(54321, 0);
-		
+
 	}
 
 	private void startServer(String port) throws IOException {
 
 		@SuppressWarnings("resource")
 		ServerSocket socket = new ServerSocket(Integer.parseInt(port));
-		
+
 		while(true) {
 			new SimpleServer(socket.accept()).start();
 		}
-		
+
 	}
-	
+
 	class SimpleServer extends Thread{
-		
+
 		private Socket socket = null;
 
 		public SimpleServer(Socket accept) {
 			this.socket = accept;
 		}
-		
+
 		public void run() {
 			try {
+				if(socket.getPort() == 8000) {
+
+				}
 				String stream = null;
 				ObjectOutputStream outStream = new ObjectOutputStream(socket.getOutputStream());
 				ObjectInputStream inStream = new ObjectInputStream(socket.getInputStream());
-				
-				//TODO menu
-				
-				outStream.writeObject(getStreams());
-				
-				stream = (String)inStream.readObject();
-				
-				while(stream == null) {} //e preciso?
-				
-				outStream.writeObject(connectTo(stream));				
-				
+
+				int action = (int) inStream.readObject();
+
+				switch (action) {
+				case 1:
+					outStream.writeObject(getStreams());
+
+					stream = (String)inStream.readObject();
+					while(stream == null) {} //e preciso?
+
+					outStream.writeObject(connectTo(stream));
+					break;
+
+				case 2:
+					addHost(inStream, outStream);
+					break;
+
+				case 3:
+					String info = (String)inStream.readObject();
+					removeClient(info);
+
+				default:
+					break;
+				}
+
 				socket.close();
 				outStream.close();
 				inStream.close();
@@ -84,41 +102,68 @@ public class Server {
 		}
 
 		private String connectTo(String stream) {
-			
+
 			int userId = Integer.parseInt(stream);
-			
+
 			List<String> ips = transmiters.get(userId);
-			
+
 			String result = ips.get(counter.get(userId));
-			transmiters.get(userId).add(socket.getRemoteSocketAddress().toString());
+			transmiters.get(userId).add(socket.getRemoteSocketAddress().toString()+":"+(socket.getPort()+1));
 			counter.replace(userId, counter.get(userId)+1);
-			
+
 			return result;
 		}
 
 		private String getStreams() {
 
 			StringBuilder sb = new StringBuilder();
-			
+
 			for(Entry<Integer, List<String>> entry : transmiters.entrySet()) {
 				sb.append(entry.getKey() + "\n");
 			}
 
 			return sb.toString();
 		}
-		
+
 		//assumindo k user é o ip:port de alguem que esta a assistir
-		private void removeClient(int host, String user) {
-			List<String> users = transmiters.get(host);
-			users.remove(user);
-			counter.replace(host, counter.get(host)-1);
+		private void removeClient(String info) {
+			String[] data = info.split("S//+");
+
+			List<String> users = transmiters.get(Integer.parseInt(data[0]));
+			users.remove(data[1]);
+			counter.replace(Integer.parseInt(data[0]), counter.get(Integer.parseInt(data[0]))-1);
+
 		}
-		
+
+		/*
+		//TODO join in removeClient
 		private void removeHost(int user) {
 			transmiters.remove(user);
 			counter.remove(user);
 		}
-		
+		*/
+
+		private void addHost(ObjectInputStream in, ObjectOutputStream out) throws ClassNotFoundException, IOException {
+			int userId = (int)in.readObject();
+			boolean unique = uniqueUser(userId);
+
+			while(unique) {
+				out.writeObject(-1);
+				userId = (int)in.readObject();
+				unique = uniqueUser(userId);
+			}
+
+			List<String> ips = new ArrayList<String>();
+			ips.add(socket.getRemoteSocketAddress().toString());
+			transmiters.put(userId, ips);
+			counter.put(userId, 0);
+			out.writeObject(1);
+		}
+
+		private boolean uniqueUser(int userId) {
+			return !transmiters.containsKey(userId);
+		}
+
 	}
 
 }
