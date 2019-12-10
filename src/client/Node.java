6ff276bcece3a8a6ef30cd0e4e8bc890a41c01e4
Pedro Carrega <pedro.carrega@outlook.com>
2019-12-10 11:36:53 +0000

    implementing node class

---
+++ src/client/Node.java
@@ -0,0 +1,26 @@
+package client;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class Node {
+	
+	public Socket socket;
+	public ObjectOutputStream out;
+	public ObjectInputStream in;
+	
+	public Node(Socket socket) throws IOException {
+		this.socket = socket;
+		this.in = new ObjectInputStream(this.socket.getInputStream());
+		this.out = new ObjectOutputStream(this.socket.getOutputStream());
+	}
+	
+	public Node(Socket socket, ObjectOutputStream out, ObjectInputStream in){
+		this.socket = socket;
+		this.in = in;
+		this.out = out;
+	}
+
+}
