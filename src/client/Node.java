eaf8b6c5da10146e2b63517be049f120a9779f00
Pedro Carrega <pedro.carrega@outlook.com>
2019-12-15 22:02:51 +0000

    node now has watching

--- src/client/Node.java
+++ src/client/Node.java
@@ -10,26 +10,31 @@ public class Node {
 	private Socket socket;
 	private ObjectOutputStream out;
 	private ObjectInputStream in;
+	private boolean watching;
 
 	public Node(Socket socket) throws IOException {
 		this.socket = socket;
 		this.out = new ObjectOutputStream(this.socket.getOutputStream());
 		this.in = new ObjectInputStream(this.socket.getInputStream());
+		this.watching = false;
 	}
 	
-	public Node(Socket socket, ObjectOutputStream out, ObjectInputStream in){
+	public Node(Socket socket, boolean watch){
 		this.socket = socket;
-		this.in = in;
-		this.out = out;
+		this.watching = watch;
 	}
 
 	public void close() throws IOException {
 		out.close();
 		in.close();
 		socket.close();
-		
 	}
 	
+
+	public boolean isWatching() {
+		return watching;
+	}
+
 	public void randomWalk(String msg) throws IOException {
 		out.writeObject(msg);
 	}
@@ -48,6 +53,10 @@ public class Node {
 
 	public ObjectInputStream getInputStream() {
 		return in;
+	}
+
+	public void setWatching(boolean b) {
+		this.watching = b;
 	}
 
 }
